d3aeae2639bf629e16dc1e51ffaa4b864aabb1ef
Leo Wu <leo.wu@leo-wu-mbp.local>
2016-06-21 16:04:01 -0700

    added to-do list on mac_install script

--- mac_install.sh
+++ mac_install.sh
@@ -6,3 +6,6 @@ cp .bashrc ~/.bashrc
 echo "source ~/.bashrc" > ~/.bash_profile
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# to-do: make a script to install brew, get Xcode (if possible, might not be) and install macvim, fasd, tmux, brew-rmtree
+
