0fe44ad142a8250eb21f46bca76e04e3c2abed28
Leo Wu <leowu@berkeley.edu>
2016-07-02 13:48:13 -0700

    mac_install.sh now creates ~.vim/backup and ~/.vim/swap directories

--- mac_install.sh
+++ mac_install.sh
@@ -6,6 +6,8 @@ cp .bashrc ~/.bashrc
 echo "source ~/.bashrc" > ~/.bash_profile
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir ~/.vim/swap
+mkdir ~/.vim/backup
 
 # to-do: make a script to install brew, get Xcode (if possible, might not be) and install macvim, fasd, tmux, brew-rmtree
 
