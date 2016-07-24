5ca2a0d3e1895e449227af413a89a5c2aecc583f
Leo Wu <leo.wu@leo-wu-mbp.local>
2016-07-24 03:44:56 -0700

    added node and cmake to brew get files

--- mac_install.sh
+++ mac_install.sh
@@ -18,7 +18,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # install brew and some useful packages
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install tmux macvim fasd python git zsh zsh-completions
+brew install tmux macvim fasd python git zsh zsh-completions node cmake
 brew cask install google-chrome google-play-music-desktop-player slack java
 
 # get brew rmtree
