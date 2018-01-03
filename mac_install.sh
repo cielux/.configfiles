c7b445427c700c01af20d2cdf674ffdde5c33155
Leo Wu <leowu.343@gmail.com>
2018-01-03 14:24:33 -0800

    Added command line installation for Apple Command Line Tools - this doesn't seem to be installed by opening Xcode anymore

--- mac_install.sh
+++ mac_install.sh
@@ -40,6 +40,9 @@ sudo xcodebuild -license accept
 printf "\n${CYAN}If you are on a fresh install, you may need to open Xcode to obtain additional components that are required for the rest of this script. Please do so now.${NC}\n"
 read -p "Press ENTER to continue"
 
+# install Apple Command Line Tools
+xcode-select --install
+
 # fix some weird problem with Xcode and CLT
 sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
 
