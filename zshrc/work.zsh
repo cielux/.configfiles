73c31d6f9eaff50a235e6cbe44ab115b2d605b62
Leo Wu <leowu.343@gmail.com>
2018-03-08 18:26:05 -0800

    work aliases: made monty-deploy relink current node if deploy fails

--- zshrc/work.zsh
+++ zshrc/work.zsh
@@ -1,5 +1,5 @@
 # Meraki stuff
 alias devfs="mkdir -p ~/Volumes/dev108 ; sshfs leowu@dev108.meraki.com:/home/leowu/ ~/Volumes/dev108 -oauto_cache,reconnect,volname=dev108"
-alias monty-deploy="brew unlink node && brew link --force node@4 && git pull -r && grunt deploy && brew unlink node@4 && brew link node"
+alias monty-deploy="brew unlink node && brew link --force node@4 ; git pull -r && grunt deploy ; brew unlink node@4 && brew link node"
 alias salesforce-pull="git checkout wudev && git pull -r && git checkout dev && git pull -r && git checkout staging && git pull -r && git checkout master && git pull -r"
 
