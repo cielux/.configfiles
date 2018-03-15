70dfe4602e91905d2d6a787b61703f7c358282cf
Leo Wu <leowu.343@gmail.com>
2018-03-15 16:44:58 -0700

    Moved brew aliases to mac.zsh config

--- zshrc/mac.zsh
+++ zshrc/mac.zsh
@@ -1,3 +1,8 @@
 # necessary for macOS Sierra's ssh key forwarding to work
 ssh-add -K &> /dev/null
 
+# brew shortcuts
+alias brew-update="brew update && brew upgrade"
+alias brew-cleanup="brew cleanup && brew cleanup -s && brew cask cleanup"
+alias brew-brew="brew-update && brew-cleanup"
+
--- zshrc/zshrc.zsh
+++ zshrc/zshrc.zsh
@@ -5,11 +5,6 @@ alias v="f -e vim"
 # fasd at startup
 eval "$(fasd --init auto)"
 
-# brew shortcuts
-alias brew-update="brew update && brew upgrade"
-alias brew-cleanup="brew cleanup && brew cleanup -s && brew cask cleanup"
-alias brew-brew="brew-update && brew-cleanup"
-
 # start vim mode
 bindkey -v
 
