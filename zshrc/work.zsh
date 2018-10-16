182603308c888dadf56e6efa53e810f1314489b3
Leo Wu <leo.wu@meraki.com>
2018-10-15 17:57:34 -0700

    Added -f to refs function export

--- zshrc/work.zsh
+++ zshrc/work.zsh
@@ -1,6 +1,6 @@
 # Meraki stuff
 alias devfs="mkdir -p ~/Volumes/dev121 ; sshfs leowu@dev121.meraki.com:/home/leowu/ ~/Volumes/dev121 -oauto_cache,reconnect,volname=dev121"
-export refs
+export -f refs
 alias monty-deploy="brew unlink node && brew link --overwrite --force node@4 ; git pull -r && grunt deploy ; brew unlink node@4 && brew link node"
 alias salesforce-pull="git checkout wudev && git pull -r && git checkout dev && git pull -r && git checkout staging && git pull -r && git checkout master && git pull -r"
 
