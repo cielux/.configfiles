9d71841f9fe860e3412b531a6f0d8a4bdb3acd0a
Leo Wu <leo.wu@meraki.com>
2018-10-25 17:55:24 -0700

    Updated aliases for corpweb deploys

--- zshrc/work.zsh
+++ zshrc/work.zsh
@@ -1,8 +1,12 @@
 # Meraki stuff
 alias devfs="mkdir -p ~/Volumes/dev121 ; sshfs leowu@dev121.meraki.com:/home/leowu/ ~/Volumes/dev121 -oauto_cache,reconnect,volname=dev121"
 export -f refs
-alias monty-deploy="brew unlink node && brew link --overwrite --force node@4 ; git pull -r && grunt deploy ; brew unlink node@4 && brew link node"
 alias salesforce-pull="git checkout wudev && git pull -r && git checkout dev && git pull -r && git checkout staging && git pull -r && git checkout master && git pull -r"
+
+alias link-node4="brew unlink node ; brew link --overwrite --force node@4"
+alias link-node="brew unlink node@4 ; brew link node"
+alias monty-deploy="link-node4 ; git pull -r && grunt deploy ; link-node"
+alias monty-deploy-staging="link-node4 ; git pull -r && grunt deploy-staging ; link-node"
 
 refs() {
   sudo umount -f ~/Volumes/dev121
