0ad9754f78f9a973554d33ea03026e8481b222e5
Andrew <andrewngorain@gmail.com>
2019-01-08 14:53:21 -0800

    Delete mac.zsh

--- zshrc/mac.zsh
+++
@@ -1,8 +0,0 @@
-# necessary for macOS Sierra's ssh key forwarding to work
-ssh-add -K &> /dev/null
-
-# brew shortcuts
-alias brew-update="brew update && brew upgrade"
-alias brew-cleanup="brew cleanup && brew cleanup -s"
-alias brew-brew="brew-update && brew-cleanup"
-
